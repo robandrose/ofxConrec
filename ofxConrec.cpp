67b24e883771ab1b5c48baa195f4cced3abe82c3
Matthias Rohrbach <m.rohrbach@robandrose.ch>
2014-09-21 17:39:54 +0200

    changes in upperbound, weird behaviour of switched w and h is now gone
    Signed-off-by: Matthias Rohrbach <m.rohrbach@robandrose.ch>

--- ofxConrec.cpp
+++ ofxConrec.cpp
@@ -27,47 +27,34 @@ void ofxConrec::setSize(int _width, int _height){
 
 
 void ofxConrec::init(int _lowest, int _highest, int _nlevels){
-    
     nlevels=_nlevels;
     lowest=_lowest;
     highest=_highest;
-    
+    mesh.clear();
     float levelstep=(float)(highest-lowest)/nlevels;
-    
     cmap=new CContourMap();
     nlevels=cmap->generate_levels(lowest, highest, nlevels);
     cmap->contour(this);
-    
     ofFloatColor linec;
     linec.setHsb(1, 1,1);
-    
     for(int i=0;i<nlevels;i++){
         CContourLevel* level=cmap->level(i);
-        
         linec.setHue(i/50.0f);
-        
         if(level!=NULL){
-            
             for(int h=0;h<level->raw->size();h++){
                 SPair pair=level->raw->at(h);
-                
                 mesh.addColor(linec);
                 mesh.addVertex(ofVec3f(pair.p1.x, pair.p1.y,i*levelstep));
-                
                 mesh.addColor(linec);
                 mesh.addVertex(ofVec3f(pair.p2.x, pair.p2.y,i*levelstep));
-                
             }
         }
     }
 }
 
 void ofxConrec::draw(){
-    ofSetColor(255,255,255);
     mesh.draw();
-    
 }
-
 
 double ofxConrec::value(double _x, double _y){
     int index=_x+_y*width;
@@ -75,7 +62,7 @@ double ofxConrec::value(double _x, double _y){
 }
 
 SPoint ofxConrec::upper_bound(){
-    return SPoint(width-1,height-1);
+    return SPoint(height-1,width-1);
     
 }
 SPoint ofxConrec::lower_bound(){
